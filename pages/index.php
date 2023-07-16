319dc06a5bd7c5b932edc9fa853215ecd4f27bbb
steven <rojasguerreros185@gmail.com>
2023-07-15 21:59:53 -0500

    terminar de validar

--- pages/index.php
+++ pages/index.php
@@ -4,7 +4,7 @@ include('../conexion.php');
 if(isset($_SESSION['DBid'])==false) header("location:../index.php");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +14,7 @@ if(isset($_SESSION['DBid'])==false) header("location:../index.php");
 </head>
 <body>
     <h1>Bienvenido,
-        <?php echo $_SESSION['DBnombre']."4 ". $_SESSION['DBapellido'];?> 
+        <?php echo $_SESSION['DBnombre']." ". $_SESSION['DBapellido'];?> 
     </h1>
     <form method="post" action="cerrar_sesion.php">
         <button type="submit" name="btn_cerrar" class="btn btn-success">Cerrar Sesion</button>
