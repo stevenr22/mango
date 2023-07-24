98f99ec038e3eba0a6bdea024a0a7a45d3db80e8
stevenr22 <rojasguerreros185@gmail.com>
2023-07-23 20:30:55 -0500

    confirma

--- pages/actualizar_perfil.php
+++ pages/actualizar_perfil.php
@@ -3,13 +3,14 @@
 include("../conexion.php");
 
 // Paso 2: Obtener datos del formulario (suponiendo que el formulario tiene un campo 'id' para identificar el registro a actualizar)
-if (isset($_POST['id']) && isset($_POST['Nnom']) && isset($_POST['Nape'])) {
+if (isset($_POST['id']) && isset($_POST['Nnom']) && isset($_POST['Nape'])&& isset($_POST['Nnom_usu'])) {
     $id = $_POST['id'];
     $Nnom = $_POST['Nnom'];
     $Nape = $_POST['Nape'];
+    $Nnom_usu = $_POST['Nnom_usu'];
 
     // Paso 3: Actualizar los datos en la base de datos
-    $sql = "UPDATE usuario SET nombre='$Nnom', apellido='$Nape' WHERE id_usuario=$id";
+    $sql = "UPDATE usuario SET nombre='$Nnom', apellido='$Nape', nomb_usuario='$Nnom_usu' WHERE id_usuario=$id";
 
     if ($conn->query($sql) === TRUE) {
         ?>
