b9ccd2cdeee7a809a37d31f40c1deb61ebacae14
steven <rojasguerreros185@gmail.com>
2023-07-25 23:55:41 -0500

    hj

--- pages/reportes.php
+++ pages/reportes.php
@@ -43,28 +43,51 @@ if($_SESSION['idrol']==2){
                                 <thead style="background-color: #a66813;border-radius: 5px;color:white;">
                                     <tr>
                                         <th><b>ID</b></th>
-                                        <th><b>Descripción</b></th>
+                                        <th><b>Descripción de la actividad</b></th>
                                         <th><b>Datos del empleado</b></th>
                                         <th><b>Cargo</b></th>
-                                        <th><b>Actividad realizada</b></th>
-                                        <th><b>Observaciones</b></th>
-                                        <th><b>Fecha de la cosecha</b></th>
-                                        <th><b>Reporte</b></th>
+                                        <th><b>Cantidad de la cosecha</b></th>
+                                        <th><b>Material utilizado</b></th>
+                                        <th><b>Variedad de mango</b></th>
+                                        <th><b>Parcela aplicada</b></th>
+
+                                        <th><b>Fecha de actividad</b></th>
+                                        <th><b>Fecha inicio de la cosecha</b></th>
+                                        <th><b>Fecha fin de la cosecha</b></th>
+                                        <th><b>Operacion</b></th>
+
+
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    <?php include("../conexion.php");
+                                    $sent = "SELECT p.id_produccion, p.descripcion, u.nombre,u.apellido, 
+                                    r.Nombre_rol,p.cantidad_cosecha, m.nombre_mat,man.variedad_mango,par.nombre_parcela,p.fecha_inic_actividad, p.fecha_inic_cosecha, p.fecha_fin_cosecha 
+                                    from produccion as p, usuario as u, roles as r, materiales as m,mango as man,parcelas as par 
+                                    where p.id_usuario = u.id_usuario AND p.id_mat = m.id_mat AND p.id_mango=man.id_mango AND p.id_parcela = par.id_parcela AND r.Id_rol = u.Id_rol;";
+                                    $resp = $conn->query($sent);
+                                    while ($arr = $resp->fetch_array()){
+                                    ?>
                                     <tr>
-                                        <td>1</td>
-                                        <td>Siembra de la semilla del mango</td>
-                                        <td>Alejandro Pin</td>
-                                        <td>Empleado</td>
-                                        <td>Me encargue de sembrar la semilla del mango, en el area x, con la cantidad de agua x</td>
-                                        <td>Ninguna novedad</td>
-                                        <td>2023-02-12</td>
+                                        <td><?php echo $arr['id_produccion'] ?></td>
+                                        <td><?php echo $arr['descripcion'] ?></td>
+                                        <td><?php echo $arr['nombre']." ".$arr['apellido'] ?></td>
+                                        <td><?php echo $arr['Nombre_rol'] ?></td>
+                                        <td><?php echo $arr['cantidad_cosecha'] ?></td>
+                                        <td><?php echo $arr['nombre_mat'] ?></td>
+                                        <td><?php echo $arr['variedad_mango'] ?></td>
+
+                                        <td><?php echo $arr['nombre_parcela'] ?></td>
+                                        <td><?php echo $arr['fecha_inic_actividad'] ?></td>
+                                        <td><?php echo $arr['fecha_inic_cosecha'] ?></td>
+                                        <td><?php echo $arr['fecha_fin_cosecha'] ?></td>
                                             <td class="center">
                                                 <button type="button" class="btn btn-warning"> <i  class="material-icons">delete</i>Eliminar</button>
                                             </td>
                                         </tr>
+                                    <?php } ?>
+                                    
+                                    
                                 </tbody>
                             </table>
                         </div>
